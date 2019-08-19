2daacb32bfa78d8b824a3de30144b6960cdc1fbe
pwvendetta <jpwvendettaw@gmail.com>
2019-08-19 10:16:09 -0300

    19.08 - 1a aula J
    19.08 - 1a aula J

--- paginas/FormMoveis.php
+++ paginas/FormMoveis.php
@@ -15,7 +15,7 @@ $con = conecta();
         <br>
         <label class="text-light">Preço: </label> <input type="number" name="preco" value="" class="form-control"><br>
 
-        <label class="text-light">Imagem </label> <input type="file" name="imagem" value="" class="form-control"><br>
+        <label class="text-light">Imagem </label> <input type="text" name="imagem" value="" class="form-control"><br>
         <br>
 
         <div class="row">
@@ -34,21 +34,59 @@ $con = conecta();
           <div class="col-md">
             <select name="estilo" class="form-control">
               <option value="">Estilo</option>
-              <option value="1"></option>
+              <option value="	Argonian	">	Argonian	</option>
+<option value="	Breton	">	Breton	</option>
+<option value="	Dark Elf	">	Dark Elf	</option>
+<option value="	High Elf	">	High Elf	</option>
+<option value="	Khajiit	">	Khajiit	</option>
+<option value="	Nord	">	Nord	</option>
+<option value="	Orcish	">	Orcish	</option>
+<option value="	Redguard	">	Redguard	</option>
+<option value="	Imperial	">	Imperial	</option>
+<option value="	Common	">	Common	</option>
+<option value="	Rough	">	Rough	</option>
+<option value="	Daedric	">	Daedric	</option>
+<option value="	Clockwork	">	Clockwork	</option>
+<option value="	Dwarven	">	Dwarven	</option>
+<option value="	Fabricant	">	Fabricant	</option>
+<option value="	Dres	">	Dres	</option>
+<option value="	Hlaalu	">	Hlaalu	</option>
+<option value="	Indoril	">	Indoril	</option>
+<option value="	Redoran	">	Redoran	</option>
+<option value="	Telvanni	">	Telvanni	</option>
+<option value="	Alinor	">	Alinor	</option>
+<option value="	Murkmire	">	Murkmire	</option>
+<option value="	Elsweyr	">	Elsweyr	</option>
+<option value="	Hakoshae	">	Hakoshae	</option>
+<option value="	Ashlander	">	Ashlander	</option>
+<option value="	Jester	">	Jester	</option>
+<option value="	Velothi	">	Velothi	</option>
             </select>
 
           </div>
           <div class="col-md">
             <select name="categoria" class="form-control">
               <option value="">Categoria</option>
-              <option value="1"></option>
+              <option value="	Conservatory	">	Conservatory	</option>
+<option value="Courtyard">	Courtyard	</option>
+<option value="Hearth">	Hearth	</option>
+<option value="Dining">	Dining	</option>
+<option value="Gallery">	Gallery	</option>
+<option value="Library">	Library	</option>
+<option value="Lighting">	Lighting	</option>
+<option value="Miscellaneous">	Miscellaneous	</option>
+<option value="Parlor">	Parlor	</option>
+<option value="Services">	Services	</option>
+<option value="Suite">	Suite	</option>
+<option value="Undercroft">	Undercroft	</option>
+<option value="Workshop">	Workshop	</option>
             </select>
           </div>
 
           <div class="col-md">
             <select name="subcategoria" class="form-control">
               <option value="">Sub-Categoria</option>
-              <option value="1"></option>
+                <!-- https://eso.mmo-fashion.com/furnishings-by-type/ -->
             </select>
           </div>
         </div>
