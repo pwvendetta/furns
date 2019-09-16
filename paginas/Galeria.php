b02cbe3ddbb4f81774bcf9fcf9b42605f56d5964
Aluno <jpwvendettaw@gmail.com>
2019-09-16 11:10:25 -0300

    novo sql galeria com novas img

--- paginas/Galeria.php
+++ paginas/Galeria.php
@@ -11,14 +11,10 @@
 				$con = mysqli_connect('127.0.0.1', 'root', '', 'furns');
 				$res = mysqli_query ($con, 'SELECT * FROM movel');
 				while ($movel = mysqli_fetch_assoc($res)):?>
-    <img src="<?php echo $movel['imagem']; ?>" weight="211px" height="180px" alt="<?php echo $movel['nome']; ?>" class="imgal-img">
+    <img src="<?php echo $movel['imagem']; ?>"  alt="<?php echo $movel['nome']; ?>" class="imgal-img">
 
 			<?php endwhile; ?>
 
-    <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js"
-    integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT"
-    crossorigin="anonymous">
-    </script> -->
     </div>
 
     </div>
--- sql.sql
+++ sql.sql
@@ -1,146 +1,254 @@
-﻿-- Criação
-CREATE TABLE Pacote (
-                idPacote INT AUTO_INCREMENT NOT NULL,
-                nome VARCHAR(64) NOT NULL,
-                preco INT NOT NULL,
-                estilo VARCHAR(16) NOT NULL,
-                categoria VARCHAR(16) NOT NULL,
-                PRIMARY KEY (idPacote)
-);
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 16-Set-2019 às 15:42
+-- Versão do servidor: 10.1.40-MariaDB
+-- versão do PHP: 7.3.5
 
-CREATE TABLE Movel (
-                idMovel INT AUTO_INCREMENT NOT NULL,
-                nome VARCHAR(64) NOT NULL,
-                preco INT NOT NULL,
-                estilo VARCHAR(16) NOT NULL,
-                categoria VARCHAR(16) NOT NULL,
-                qualidade INT NOT NULL,
-                subcategoria VARCHAR(17) NOT NULL,
-                PRIMARY KEY (idMovel)
-);
-
-CREATE TABLE MovelPacote (
-                idMovel INT NOT NULL,
-                idPacote INT NOT NULL,
-                quantidade INT NOT NULL,
-                PRIMARY KEY (idMovel, idPacote)
-);
-
-CREATE TABLE Usuario (
-                idUsuario INT AUTO_INCREMENT NOT NULL,
-                conta VARCHAR(32) NOT NULL,
-                senha VARCHAR(32) NOT NULL,
-                nickname VARCHAR(32) NOT NULL,
-                email VARCHAR(127) NOT NULL,
-                PRIMARY KEY (idUsuario)
-);
-
-CREATE TABLE Venda (
-                idVenda INT AUTO_INCREMENT NOT NULL,
-                data DATE NOT NULL,
-                idUsuario INT NOT NULL,
-                PRIMARY KEY (idVenda)
-);
-
-CREATE TABLE PacoteVenda (
-                idVenda INT NOT NULL,
-                idPacote INT NOT NULL,
-                quantidade INT NOT NULL,
-                PRIMARY KEY (idVenda, idPacote)
-);
-
-CREATE TABLE MovelVenda (
-                idVenda INT NOT NULL,
-                idMovel INT NOT NULL,
-                quantidade INT NOT NULL,
-                PRIMARY KEY (idVenda, idMovel)
-);
-
-ALTER TABLE PacoteVenda ADD CONSTRAINT pacote_pacotesvenda_fk
-FOREIGN KEY (idPacote)
-REFERENCES Pacote (idPacote)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE MovelPacote ADD CONSTRAINT pacote_movelpacote_fk
-FOREIGN KEY (idPacote)
-REFERENCES Pacote (idPacote)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE MovelPacote ADD CONSTRAINT movel_movelpacote_fk
-FOREIGN KEY (idMovel)
-REFERENCES Movel (idMovel)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE MovelVenda ADD CONSTRAINT movel_movelvenda_fk
-FOREIGN KEY (idMovel)
-REFERENCES Movel (idMovel)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE Venda ADD CONSTRAINT usuario_venda_fk
-FOREIGN KEY (idUsuario)
-REFERENCES Usuario (idUsuario)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE MovelVenda ADD CONSTRAINT venda_moveisvenda_fk
-FOREIGN KEY (idVenda)
-REFERENCES Venda (idVenda)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE PacoteVenda ADD CONSTRAINT venda_pacotesvenda_fk
-FOREIGN KEY (idVenda)
-REFERENCES Venda (idVenda)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- Dados Exemplo
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO `usuario` (`idUsuario`, `conta`, `senha`, `nickname`, `email`)
-VALUES (NULL, 'pwvendetta', '12345678', 'pwvendetta', 'vendetta@email.pw');
+--
+-- Database: `furns`
+--
 
+-- --------------------------------------------------------
 
-INSERT INTO `movel` (`idMovel`, `nome`, `preco`, `estilo`, `categoria`, `qualidade`, `subcategoria`)
-VALUES (NULL, 'Alinor Bread Basket, Wrought Iron', '2000', 'Alinor', 'Hearth', '3', 'Baskets and Bags');
+--
+-- Estrutura da tabela `movel`
+--
 
-INSERT INTO `movel` (`idMovel`, `nome`, `preco`, `estilo`, `categoria`, `qualidade`, `subcategoria`)
-VALUES (NULL, 'Bread, Braided', '2000', 'Common', 'Hearth', '3', 'Breads and Desserts');
+CREATE TABLE `movel` (
+  `idMovel` int(11) NOT NULL,
+  `nome` varchar(64) NOT NULL,
+  `preco` int(11) NOT NULL,
+  `estilo` varchar(16) NOT NULL,
+  `categoria` varchar(16) NOT NULL,
+  `qualidade` int(11) NOT NULL,
+  `subcategoria` varchar(19) NOT NULL,
+  `imagem` varchar(1023) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `movel` (`idMovel`, `nome`, `preco`, `estilo`, `categoria`, `qualidade`, `subcategoria`)
-VALUES (NULL, 'Bread, Hearty Loaves', '2000', 'Common', 'Hearth', '3', 'Breads and Desserts');
+--
+-- Extraindo dados da tabela `movel`
+--
 
-INSERT INTO `movel` (`idMovel`, `nome`, `preco`, `estilo`, `categoria`, `qualidade`, `subcategoria`)
-VALUES (NULL, 'Hearty Bread', '2000', 'Common', 'Hearth', '3', 'Breads and Desserts');
+INSERT INTO `movel` (`idMovel`, `nome`, `preco`, `estilo`, `categoria`, `qualidade`, `subcategoria`, `imagem`) VALUES
+(1, 'Alinor Bread Basket, Wrought Iron', 2000, 'Alinor', 'Hearth', 1, 'Baskets and Bags', 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2018/05/High-Elf-Bread-Basket-Wrought-Iron.jpg'),
+(2, 'Bread, Braided', 2000, 'Common', 'Hearth', 2, 'Breads and Desserts', 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2017/02/Bread-Braided.jpg'),
+(3, 'Bread, Hearty Loaves', 2000, 'Common', 'Hearth', 3, 'Breads and Desserts', 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2017/02/Bread-Hearty-Loaf.jpg'),
+(4, 'Hearty Bread', 2000, '	Common	', 'Hearth', 3, '', 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2017/02/Hearty-Bread.jpg'),
+(12, 'Alinor Bed, Overhang Full', 5000, '	Alinor	', 'Suite', 4, '', 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2018/05/High-Elf-Bed-Overhang-Full.jpg'),
+(13, 'Breton Throne', 30000, '	Breton	', 'Gallery', 5, '', 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2017/02/Breton-Throne.jpg'),
+(16, 'Alinor Meal, Individual', 5600, '	Alinor	', 'Hearth', 2, '', 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2018/05/High-Elf-Meal-Individual.jpg'),
+(17, 'Dark Elf Wardrobe, Scaled', 3000, '	Dark Elf	', 'Suite', 3, '', 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2017/02/Dark-Elf-Wardrobe-Scaled.jpg'),
+(19, 'Rough Platform, Stage', 300, '	Common	', 'Miscellaneous', 1, '', 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2017/01/Rough-Platform-Stage.jpg'),
+(21, 'Argonian Basket, Serving', 1000, '	Argonian	', '', 2, '', '---'),
+(23, 'Argonian Curtain of the Nest', 10000, '	Daedric	', 'Library', 3, '', 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2017/01/Argonian-Curtain-of-the-Nest.jpg');
 
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `movelpacote`
+--
 
-INSERT INTO `pacote` (`idPacote`, `nome`, `preco`, `estilo`, `categoria`)
-VALUES (NULL, 'Simple Bread Basket', '10000', 'Common', 'Hearth');
+CREATE TABLE `movelpacote` (
+  `idMovel` int(11) NOT NULL,
+  `idPacote` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-INSERT INTO `movelpacote` (`idMovel`, `idPacote`, `quantidade`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(4, 1, 1);
+--
+-- Estrutura da tabela `movelvenda`
+--
 
+CREATE TABLE `movelvenda` (
+  `idVenda` int(11) NOT NULL,
+  `idMovel` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `venda` (`idVenda`, `data`, `idUsuario`) VALUES (NULL, '2019-08-06', '1'), (NULL, '2019-08-06', '1');
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `pacote`
+--
 
-INSERT INTO `pacotevenda` (`idVenda`, `idPacote`, `quantidade`) VALUES ('1', '1', '1');
-INSERT INTO `movelvenda` (`idVenda`, `idMovel`, `quantidade`) VALUES ('2', '3', '2');
+CREATE TABLE `pacote` (
+  `idPacote` int(11) NOT NULL,
+  `nome` varchar(64) NOT NULL,
+  `preco` int(11) NOT NULL,
+  `estilo` varchar(16) NOT NULL,
+  `categoria` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-ALTER TABLE `movel` ADD `imagem` VARCHAR(9999) NOT NULL AFTER `subcategoria`;
+--
+-- Estrutura da tabela `pacotevenda`
+--
 
+CREATE TABLE `pacotevenda` (
+  `idVenda` int(11) NOT NULL,
+  `idPacote` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-UPDATE `movel` SET `Imagem` = 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2018/05/High-Elf-Bread-Basket-Wrought-Iron.jpg' WHERE `movel`.`idMovel` = 1;
-UPDATE `movel` SET `Imagem` = 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2017/02/Bread-Braided.jpg' WHERE `movel`.`idMovel` = 2;
-UPDATE `movel` SET `Imagem` = 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2017/02/Bread-Hearty-Loaf.jpg' WHERE `movel`.`idMovel` = 3;
-UPDATE `movel` SET `Imagem` = 'https://eso.mmo-fashion.com/wp-content/uploads/sites/2/2017/02/Hearty-Bread.jpg' WHERE `movel`.`idMovel` = 4;
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `conta` varchar(32) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  `nickname` varchar(32) NOT NULL,
+  `email` varchar(127) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `conta`, `senha`, `nickname`, `email`) VALUES
+(1, 'pwvendetta', '12345678', 'pwvendetta', 'vendetta@email.pw');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+CREATE TABLE `venda` (
+  `idVenda` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `idUsuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `movel`
+--
+ALTER TABLE `movel`
+  ADD PRIMARY KEY (`idMovel`);
+
+--
+-- Indexes for table `movelpacote`
+--
+ALTER TABLE `movelpacote`
+  ADD PRIMARY KEY (`idMovel`,`idPacote`),
+  ADD KEY `pacote_movelpacote_fk` (`idPacote`);
+
+--
+-- Indexes for table `movelvenda`
+--
+ALTER TABLE `movelvenda`
+  ADD PRIMARY KEY (`idVenda`,`idMovel`),
+  ADD KEY `movel_movelvenda_fk` (`idMovel`);
+
+--
+-- Indexes for table `pacote`
+--
+ALTER TABLE `pacote`
+  ADD PRIMARY KEY (`idPacote`);
+
+--
+-- Indexes for table `pacotevenda`
+--
+ALTER TABLE `pacotevenda`
+  ADD PRIMARY KEY (`idVenda`,`idPacote`),
+  ADD KEY `pacote_pacotesvenda_fk` (`idPacote`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- Indexes for table `venda`
+--
+ALTER TABLE `venda`
+  ADD PRIMARY KEY (`idVenda`),
+  ADD KEY `usuario_venda_fk` (`idUsuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `movel`
+--
+ALTER TABLE `movel`
+  MODIFY `idMovel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `pacote`
+--
+ALTER TABLE `pacote`
+  MODIFY `idPacote` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `venda`
+--
+ALTER TABLE `venda`
+  MODIFY `idVenda` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `movelpacote`
+--
+ALTER TABLE `movelpacote`
+  ADD CONSTRAINT `movel_movelpacote_fk` FOREIGN KEY (`idMovel`) REFERENCES `movel` (`idMovel`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pacote_movelpacote_fk` FOREIGN KEY (`idPacote`) REFERENCES `pacote` (`idPacote`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `movelvenda`
+--
+ALTER TABLE `movelvenda`
+  ADD CONSTRAINT `movel_movelvenda_fk` FOREIGN KEY (`idMovel`) REFERENCES `movel` (`idMovel`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `venda_moveisvenda_fk` FOREIGN KEY (`idVenda`) REFERENCES `venda` (`idVenda`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `pacotevenda`
+--
+ALTER TABLE `pacotevenda`
+  ADD CONSTRAINT `pacote_pacotesvenda_fk` FOREIGN KEY (`idPacote`) REFERENCES `pacote` (`idPacote`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `venda_pacotesvenda_fk` FOREIGN KEY (`idVenda`) REFERENCES `venda` (`idVenda`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD CONSTRAINT `usuario_venda_fk` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
